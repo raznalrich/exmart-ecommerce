db524f35d9d54d185b22fd9d822703b0b7d4b0bd
Mohammed Rasnal K A <mohammed.ka@experionglobal.local>
2024-11-30 12:31:57 +0530

    folder structure

---
+++ src/app/layout/user/ui/usernavbar/usernavbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-usernavbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './usernavbar.component.html',
+  styleUrl: './usernavbar.component.scss'
+})
+export class UsernavbarComponent {
+
+}
