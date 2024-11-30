d9632016af28455acb690e3f74a8e196ffc0816b
Akshay Vijayakumar <akshay.vijayakumar@experionglobal.local>
2024-11-30 16:03:06 +0530

    new:branch-addtocart

---
+++ src/app/layout/user/pages/addtocartpage/addtocartpage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-addtocartpage',
+  standalone: true,
+  imports: [],
+  templateUrl: './addtocartpage.component.html',
+  styleUrl: './addtocartpage.component.scss'
+})
+export class AddtocartpageComponent {
+
+}
