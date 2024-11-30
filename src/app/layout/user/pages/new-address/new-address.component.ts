f6e0f0df8c1e963c1221573e6d70262d4f549330
arunsankarcm <sankarchevoor@gmail.com>
2024-11-30 19:52:27 +0530

    profile page created

---
+++ src/app/layout/user/pages/new-address/new-address.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-address',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-address.component.html',
+  styleUrl: './new-address.component.scss'
+})
+export class NewAddressComponent {
+
+}
