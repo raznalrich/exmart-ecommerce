4cd7f79bc684be2093d312c507341ef9ca1e9c1e
Akshay Vijayakumar <akshay.vijayakumar@experionglobal.local>
2024-12-02 19:06:29 +0530

    New:addtocart page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AddtocartpageComponent } from './layout/user/pages/addtocartpage/addtocartpage.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AddtocartpageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/layout/user/pages/addtocartpage/addtocartpage.component.ts
+++ src/app/layout/user/pages/addtocartpage/addtocartpage.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CheckoutbuttonComponent } from "../ui/checkoutbutton/checkoutbutton.component";
 
 @Component({
   selector: 'app-addtocartpage',
   standalone: true,
-  imports: [],
+  imports: [AddtocartpageComponent, CheckoutbuttonComponent],
   templateUrl: './addtocartpage.component.html',
   styleUrl: './addtocartpage.component.scss'
 })
