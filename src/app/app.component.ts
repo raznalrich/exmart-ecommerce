12ac6a77ac46ef03b796821519e82ba913c1a7b7
arunsankarcm <sankarchevoor@gmail.com>
2024-12-04 09:23:39 +0530

    select address, create address, order history

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProfileComponent } from "./layout/user/pages/profile/profile.component";
+import { NewAddressComponent } from "./layout/user/pages/new-address/new-address.component";
+import { SelectAddressComponent } from "./layout/user/pages/select-address/select-address.component";
+import { UserOrdersComponent } from "./layout/user/pages/user-orders/user-orders.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ ProfileComponent],
+  imports: [NewAddressComponent, SelectAddressComponent, UserOrdersComponent, ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
