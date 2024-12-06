8f0f134b495a0623ffc449121ce4d53c2d1d4da8
Mohammed Rasnal K A <mohammed.ka@experionglobal.local>
2024-12-06 10:12:03 +0530

    routes of admin page and admin pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { HomeStaticComponent } from './layout/user/pages/home-static/home-static.component';
-=======
 import { HomepageComponent } from "./layout/user/homepage/homepage.component";
 import { UsernavbarComponent } from "./layout/user/ui/usernavbar/usernavbar.component";
 import { SidebarComponent } from "./layout/admin/ui/sidebar/sidebar.component";
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,47 @@ import { HomepageComponent } from './layout/user/homepage/homepage.component';
 import { Routes } from '@angular/router';
 
 import { SingleproductpageComponent } from './layout/user/pages/singleproductpage/singleproductpage.component';
+import { HomeStaticComponent } from './layout/user/pages/home-static/home-static.component';
+import { DashboardComponent } from './layout/admin/dashboard/dashboard.component';
+import { AdminDashboardComponent } from './layout/admin/pages/admin-dashboard/admin-dashboard.component';
+import { ProductlistComponent } from './layout/admin/pages/productlist/productlist.component';
+import { OrderListComponent } from './layout/admin/pages/order-list/order-list.component';
+import { ReportPageComponent } from './layout/admin/pages/report-page/report-page.component';
+import { ViewFeedbackComponent } from './layout/admin/pages/view-feedback/view-feedback.component';
+import { LoginComponent } from './layout/authentication/login/login.component';
 
 export const routes: Routes = [
   {
-    path:'',component: HomepageComponent
+    path:'',component: HomepageComponent,children:[
+      {
+        path:'home', component: HomeStaticComponent
+      },
+      {
+        path:'viewproduct/:id',component:SingleproductpageComponent
+      }
+    ]
   },
+  {
+    path:'admin',component: DashboardComponent,children:[
+      {
+        path:'admindashboard',component:AdminDashboardComponent
+      },
+      {
+        path:'productlist',component:ProductlistComponent
+      },
+      {
+        path:'orderlist',component:OrderListComponent
+      },
+      {
+        path:'reportpage',component:ReportPageComponent
+      },
+      {
+        path:'viewfeedback',component:ViewFeedbackComponent
+      }
+    ]
+  },
+  {
+    path:'login',component:LoginComponent
+  }
 
 ];
