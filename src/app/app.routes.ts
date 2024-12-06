67b7111c153373b37320628fd502bc316a7dcd7e
sheethal-p-hari <sheethalphari@gmail.com>
2024-12-06 18:47:24 +0530

    single-page changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -18,7 +18,7 @@ export const routes: Routes = [
         path:'home', component: HomeStaticComponent
       },
       {
-        path:'viewproduct/:id',component:SingleproductpageComponent
+        path:'viewproduct',component:SingleproductpageComponent
       }
     ]
   },
