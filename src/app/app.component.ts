cb0d62569e4f4f56f8d036bf2cdc952ccc66dfcf
Shalu Murali <shalu.murali@experionglobal.local>
2024-12-06 12:57:13 +0530

    product-display admin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { HomeStaticComponent } from './layout/user/pages/home-static/home-static.component';
-=======
 import { HomepageComponent } from "./layout/user/homepage/homepage.component";
 import { UsernavbarComponent } from "./layout/user/ui/usernavbar/usernavbar.component";
 import { SidebarComponent } from "./layout/admin/ui/sidebar/sidebar.component";
+import { ProductlistComponent } from "./layout/admin/pages/productlist/productlist.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
 
-  imports: [RouterOutlet, HomepageComponent, UsernavbarComponent, SidebarComponent],
+  imports: [RouterOutlet, HomepageComponent, UsernavbarComponent, SidebarComponent, ProductlistComponent],
 
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
