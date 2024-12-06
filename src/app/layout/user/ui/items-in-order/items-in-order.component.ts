28e25105afc8f66967a1c5ca7c3e85eac5c1794a
Hithesh Lal <hithesh.lal@experionglobal.local>
2024-12-06 18:31:29 +0530

    thankyoupage ui design change and json file

--- src/app/layout/user/ui/items-in-order/items-in-order.component.ts
+++ src/app/layout/user/ui/items-in-order/items-in-order.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-items-in-order',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './items-in-order.component.html',
   styleUrl: './items-in-order.component.scss'
 })
 export class ItemsInOrderComponent {
-  Orders:any;
+  @Input() orders: any;
+
+  ngOnInit(){
+    console.log(this.orders)
+  }
+
 }
