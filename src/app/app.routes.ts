74dbdc0c75cfd1ac7200db883638f45f29adc42d
Hithesh Lal <hithesh.lal@experionglobal.local>
2024-12-06 11:01:47 +0530

    Thank you page intial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ThankyoupageComponent } from './layout/user/pages/thankyoupage/thankyoupage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:"thankyou", component:ThankyoupageComponent
+  }
+];
---
+++ src/app/layout/user/pages/thankyoupage/thankyoupage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-thankyoupage',
+  standalone: true,
+  imports: [],
+  templateUrl: './thankyoupage.component.html',
+  styleUrl: './thankyoupage.component.scss'
+})
+export class ThankyoupageComponent {
+
+}
