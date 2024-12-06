e22bdc550d1a451f1e6179a09ae85eed81521e98
Hithesh Lal <hithesh.lal@experionglobal.local>
2024-12-06 18:31:29 +0530

    thankyoupage ui design change and json file

--- src/app/services/api-service.service.ts
+++ src/app/services/api-service.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,11 @@ import { Injectable } from '@angular/core';
 })
 export class ApiServiceService {
 
-  constructor() { }
+
+  constructor(public http:HttpClient) { }
+
+  getItemsInOrder() {
+    return this.http.get(`Data/OrderDetails.json`)
+  }
+
 }
