4d2f6258ebe85eef3113a674ab6d33fc86f51df3
sheethal-p-hari <sheethalphari@gmail.com>
2024-12-06 12:20:07 +0530

    single page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SingleproductpageComponent } from './layout/user/pages/singleproductpage/singleproductpage.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SingleproductpageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/layout/user/ui/single-product-detail/single-product-detail.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input } from '@angular/core';
+import { ApiService } from '../../../../api.service';
+
+@Component({
+  selector: 'app-single-product-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './single-product-detail.component.html',
+  styleUrl: './single-product-detail.component.scss'
+})
+export class SingleProductDetailComponent {
+  arr:any;
+  @Input()details:any={
+name:'',
+code:''
+    }
+  constructor(public api: ApiService) {}
+  ngOnInit() {
+    this.api.getProductDetails().subscribe((res: any) => {
+      this.details = res;
+console.log(this.details);
+
+      this.arr=this.details.products;
+      console.log(this.arr);
+    });
+}
+}
