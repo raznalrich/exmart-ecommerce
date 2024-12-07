d6fb87fa1fb1e89b6891381b603f3d2419c6202a
Akshay Vijayakumar <akshay.vijayakumar@experionglobal.local>
2024-12-07 09:18:22 +0530

    fix:conflit issues fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-<<<<<<< HEAD
+
 import { AddtocartpageComponent } from './layout/user/pages/addtocartpage/addtocartpage.component';
-=======
+
 import { ThankyoupageComponent } from './layout/user/pages/thankyoupage/thankyoupage.component';
 
 import { HomeStaticComponent } from './layout/user/pages/home-static/home-static.component';
@@ -10,18 +10,15 @@ import { HomepageComponent } from "./layout/user/homepage/homepage.component";
 import { UsernavbarComponent } from "./layout/user/ui/usernavbar/usernavbar.component";
 import { SidebarComponent } from "./layout/admin/ui/sidebar/sidebar.component";
 
->>>>>>> aeaaa3aaa3ac51f4dc1cccde8736b975089c9d10
 
 @Component({
   selector: 'app-root',
   standalone: true,
-<<<<<<< HEAD
-  imports: [RouterOutlet, AddtocartpageComponent],
-=======
 
-  imports: [RouterOutlet, HomepageComponent, UsernavbarComponent, SidebarComponent,ThankyoupageComponent],
 
->>>>>>> aeaaa3aaa3ac51f4dc1cccde8736b975089c9d10
+  imports: [RouterOutlet, HomepageComponent, UsernavbarComponent, SidebarComponent,ThankyoupageComponent,AddtocartpageComponent],
+
+
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
