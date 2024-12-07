a19f9b455a488e8bffa7bfab758077b00986b9b5
Hithesh Lal <hithesh.lal@experionglobal.local>
2024-12-07 13:07:05 +0530

    order list

--- src/app/layout/admin/pages/order-list/order-list.component.ts
+++ src/app/layout/admin/pages/order-list/order-list.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { OrderlistTableComponent } from "../../ui/orderlist-table/orderlist-table.component";
+import { SidebarComponent } from "../../ui/sidebar/sidebar.component";
+import { ApiServiceService } from '../../../../services/api-service.service';
 
 @Component({
   selector: 'app-order-list',
   standalone: true,
-  imports: [],
+  imports: [OrderlistTableComponent, SidebarComponent],
   templateUrl: './order-list.component.html',
   styleUrl: './order-list.component.scss'
 })
 export class OrderListComponent {
 
+  orderlist:any[]=[];
+
+  constructor(public api:ApiServiceService){}
+
+  ngOnInit(){
+    this.api.getOrderList().subscribe((res:any)=>{
+     this.orderlist = res;
+      console.log(this.orderlist)
+    })
+
+  }
+
 }
+
+
