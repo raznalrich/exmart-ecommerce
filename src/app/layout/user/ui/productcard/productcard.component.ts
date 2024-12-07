5e4b4bebe69858262615c98508026d9661b6020d
Mohammed Rasnal K A <mohammed.ka@experionglobal.local>
2024-12-07 09:18:38 +0530

    Add product Card

--- src/app/layout/user/ui/productcard/productcard.component.ts
+++ src/app/layout/user/ui/productcard/productcard.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ApiServiceService } from '../../../../services/api-service.service';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-productcard',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './productcard.component.html',
   styleUrl: './productcard.component.scss'
 })
 export class ProductcardComponent {
+  constructor(public api:ApiServiceService){}
 
+  @Input() cards:any={
+    category:'',
+colors:[],
+id:0,
+imageUrl:'',
+name:'',
+price:0,
+size:'',
+type:''
+  }
 }
