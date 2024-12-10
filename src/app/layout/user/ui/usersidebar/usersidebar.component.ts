3c787d5a0ed1fe84d77a70ee23c0303786fe8bef
Shalu Murali <shalu.murali@experionglobal.local>
2024-12-10 09:46:59 +0530

    feature : user sidebar

--- src/app/layout/user/ui/usersidebar/usersidebar.component.ts
+++ src/app/layout/user/ui/usersidebar/usersidebar.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-usersidebar',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './usersidebar.component.html',
   styleUrl: './usersidebar.component.scss'
 })
 export class UsersidebarComponent {
-
+  menuItems: any = [{
+    id:1,
+    routerLink:'admindashboard',
+    item:'Orders',
+    image:'bi bi-bar-chart'
+  },
+  {
+    id:2,
+    routerLink:'productlist',
+    item:'Address',
+    image:'bi bi-boxes'
+  },
+];
 }
