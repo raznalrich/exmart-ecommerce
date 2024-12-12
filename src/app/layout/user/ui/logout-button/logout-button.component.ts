2cead1ae4b326529dda7dac4c9a3077bbf082611
Shalu Murali <shalu.murali@experionglobal.local>
2024-12-12 09:27:49 +0530

    user sidebar

---
+++ src/app/layout/user/ui/logout-button/logout-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-logout-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './logout-button.component.html',
+  styleUrl: './logout-button.component.scss',
+})
+export class LogoutButtonComponent {
+  @Input() button: any = {
+    label: '',
+    icon: '',
+  };
+}
