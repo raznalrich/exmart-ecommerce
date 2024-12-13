a8ce435a47aa8dbe1cd4599adb05f7939003c27f
Akshay Vijayakumar <akshay.vijayakumar@experionglobal.local>
2024-12-13 10:10:34 +0530

    change:add to cart page redesigned

--- src/app/layout/user/pages/addtocartpage/addtocartpage.component.ts
+++ src/app/layout/user/pages/addtocartpage/addtocartpage.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CheckoutbuttonComponent } from "../../ui/checkoutbutton/checkoutbutton.component";
 import { AddtoCartDeletebtnComponent } from "../../ui/addto-cart-deletebtn/addto-cart-deletebtn.component";
 import { AddtoCartLikebtnComponent } from "../../ui/addto-cart-likebtn/addto-cart-likebtn.component";
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-addtocartpage',
   standalone: true,
-  imports: [AddtocartpageComponent, CheckoutbuttonComponent, AddtoCartDeletebtnComponent, AddtoCartLikebtnComponent],
+  imports: [AddtocartpageComponent, CheckoutbuttonComponent, AddtoCartDeletebtnComponent, AddtoCartLikebtnComponent,CurrencyPipe],
   templateUrl: './addtocartpage.component.html',
   styleUrl: './addtocartpage.component.scss'
 })
