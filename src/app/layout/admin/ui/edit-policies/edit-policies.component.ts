616fe4f6bfe9d15cd7ec16afa933653f76c7fff0
Akshay Vijayakumar <akshay.vijayakumar@experionglobal.local>
2024-12-17 00:32:36 +0530

    Added New pop-up components for admin settings page

--- src/app/layout/admin/ui/edit-policies/edit-policies.component.ts
+++ src/app/layout/admin/ui/edit-policies/edit-policies.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-edit-policies',
+  selector: 'app-edit-policiesapp-edit-policies',
   standalone: true,
   imports: [],
   templateUrl: './edit-policies.component.html',
