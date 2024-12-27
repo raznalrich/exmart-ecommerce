b2855587f2b41f422cd41078831288589b9001a2
Akshay Vijayakumar <akshay.vijayakumar@experionglobal.local>
2024-12-27 14:02:57 +0530

    Done:Add category functionality in admin settings page

--- src/app/services/api-service.service.ts
+++ src/app/services/api-service.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 
 
 import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -34,5 +35,25 @@ export class ApiServiceService {
     return this.http.get('https://localhost:7267/api/Categories');
   }
 
+  categoryDeletion(id:any){
+    this.http.delete(`https://localhost:7267/api/Categories/${id}`).subscribe(res=>{
+      console.log(res)
+    })
+  }
 
+  addCategory(item: any) {
+    let data = {
+      categoryName: item.name,  // Changed from 'name' to 'categoryName'
+      iconPath: item.icon      // Changed from 'icon' to 'iconPath'
+    };
+
+    const headers = { 'Content-Type': 'application/json' };
+
+    return this.http.post("https://localhost:7267/api/Categories", data, { headers }).pipe(
+      catchError(error => {
+        console.log('Error details:', error.error);
+        throw error;
+      })
+    );
+  }
 }
