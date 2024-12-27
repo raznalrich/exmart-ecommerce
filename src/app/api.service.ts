356d5ec0f9bcf9c5b467485431c25272b64d5ec6
Akshay Vijayakumar <akshay.vijayakumar@experionglobal.local>
2024-12-27 14:02:57 +0530

    Done:Add category functionality in admin settings page

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -5,6 +5,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ApiService {
+  addCategory(categoryData: any) {
+    throw new Error('Method not implemented.');
+  }
   getCategory() {
     throw new Error('Method not implemented.');
   }
