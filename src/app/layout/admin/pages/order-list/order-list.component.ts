623c7ab64b651df11b5803f8fe04c3dc5dc2ed5f
Hithesh Lal <hithesh.lal@experionglobal.local>
2024-12-28 12:31:21 +0530

    minor changes done in orderlist page

--- src/app/layout/admin/pages/order-list/order-list.component.ts
+++ src/app/layout/admin/pages/order-list/order-list.component.ts
@@ -5,11 +5,12 @@ import { ApiServiceService } from '../../../../services/api-service.service';
 import { SearchbarComponent } from "../../ui/searchbar/searchbar.component";
 import { AdminValuesDisplayingButtonComponent } from "../../ui/admin-values-displaying-button/admin-values-displaying-button.component";
 import { DateRangepickerComponent } from '../../ui/date-rangepicker/date-rangepicker.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-order-list',
   standalone: true,
-  imports: [OrderlistTableComponent, SearchbarComponent, AdminValuesDisplayingButtonComponent,DateRangepickerComponent],
+  imports: [OrderlistTableComponent,ReactiveFormsModule, SearchbarComponent, AdminValuesDisplayingButtonComponent,DateRangepickerComponent],
   templateUrl: './order-list.component.html',
   styleUrl: './order-list.component.scss'
 })
