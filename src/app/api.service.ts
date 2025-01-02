d3b595390ee5ab560e325bc748e7cf1163c88c62
arunsankarcm <sankarchevoor@gmail.com>
2025-01-02 20:07:13 +0530

    add product

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -40,26 +40,37 @@ export class ApiService {
   getProducts() {
     return this.http.get(`product-sample.json`);
   }
+  private baseUrl = 'https://localhost:7267/api';
 
+  addProduct(payload: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/Product/add-product`, payload);
+  }
 
-getUserAddress(){
-return this.http.get(`Data/address.json`)
+uploadImage(file: File) {
+  const formData = new FormData();
+  formData.append('file', file);
+
+  return this.http.post<{ imageUrl: string }>(`${this.baseUrl}/ImageUpload/upload-image`, formData);
 }
 
-getUserFeedback(){
-  return this.http.get(`Data/feedback.json`)
+
+  getUserAddress() {
+    return this.http.get(`Data/address.json`);
+  }
+
+  getUserFeedback() {
+    return this.http.get(`Data/feedback.json`);
   }
 
   // getProductsById(id:any){
   //   return this.http.get(`Data/productTrail.json/${id}`)
   // }
 
-  getProductsById(id:number){
+  getProductsById(id: number) {
     return this.http.get(`/Data/productsTrail.json`).pipe(
-      map((data:any)=>{
-        const filterddata = data.filter((item:any)=> item.id == id)
+      map((data: any) => {
+        const filterddata = data.filter((item: any) => item.id == id);
         return filterddata;
-
       })
     );
   }
