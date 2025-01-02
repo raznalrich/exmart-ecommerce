9d711d50146ab98067ac26595971fe35c8df4b78
sheethal-p-hari <sheethalphari@gmail.com>
2025-01-02 10:19:54 +0530

    size and color component

--- src/app/layout/user/ui/color-button/color-button.component.ts
+++ src/app/layout/user/ui/color-button/color-button.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ApiServiceService } from '../../../../services/api-service.service';
+import { ActivatedRoute } from '@angular/router';
+import { GlobalService } from '../../../../global.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-color-button',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './color-button.component.html',
-  styleUrl: './color-button.component.scss'
+  styleUrl: './color-button.component.scss',
 })
 export class ColorButtonComponent {
+  id: any;
+  data: any;
+  color: any = '';
 
+  @Input() ColorId: any;
+  paramSubscription: any;
+  constructor(
+    public api: ApiServiceService,
+    private route: ActivatedRoute,
+    public global: GlobalService
+  ) {}
+
+  ngOnInit() {
+    //Fetch color details
+    this.api.getColorById(this.ColorId).subscribe({
+      next: (colorData) => {
+        this.color = colorData;
+        console.log("Colordata",colorData);
+        this.color = this.color.colorName;
+        console.log('color', this.color);
+      },
+      error: (error) => {
+        console.error('Error fetching color:', error);
+      },
+    });
+  }
 }
