8ded17940dc0f5dbb73340b0c99b2f96a7388d3b
arunsankarcm <sankarchevoor@gmail.com>
2025-01-03 11:06:04 +0530

    removed firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-import { provideStorage, getStorage } from '@angular/fire/storage';
-import { environment } from './environment/environment';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
@@ -14,8 +11,7 @@ export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
     provideHttpClient(),
     provideAnimationsAsync(),
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideStorage(() => getStorage()), 
+ 
   ]
 
 };
