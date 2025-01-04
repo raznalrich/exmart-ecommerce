671b5f35494471b8238c8cea54b617033b799c65
Akshay Vijayakumar <akshay.vijayakumar@experionglobal.local>
2025-01-04 12:42:48 +0530

    Done:Feedback for website functionalities with integration

--- src/app/layout/admin/ui/feedback-replay/feedback-replay.component.ts
+++ src/app/layout/admin/ui/feedback-replay/feedback-replay.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-feedback-replay',
@@ -8,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrl: './feedback-replay.component.scss'
 })
 export class FeedbackReplayComponent {
+
+  @ViewChild('staticBackdrop') modal: any;
+
+  ngAfterViewInit() {
+    const backdrop = document.querySelector('.modal-backdrop');
+    if (backdrop) {
+      backdrop.addEventListener('click', (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+      });
+    }
+  }
+
+
 
 }
