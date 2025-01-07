0f3ba6cb676de54d32dad51394e893db4b638230
Akshay Vijayakumar <akshay.vijayakumar@experionglobal.local>
2025-01-07 12:59:42 +0530

    feat:filtering functionality for product and website

--- src/app/layout/admin/ui/feedback-list/feedback-list.component.ts
+++ src/app/layout/admin/ui/feedback-list/feedback-list.component.ts
@@ -1,29 +1,57 @@
-import { Component, input } from '@angular/core';
+import { Component } from '@angular/core';
 import { ApiService } from '../../../../api.service';
 import { FeedbackReplayComponent } from "../feedback-replay/feedback-replay.component";
 import { ProductFeedbackToggleComponent } from "../product-feedback-toggle/product-feedback-toggle.component";
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-feedback-list',
   standalone: true,
-  imports: [FeedbackReplayComponent, ProductFeedbackToggleComponent],
+  imports: [FeedbackReplayComponent, ProductFeedbackToggleComponent,CommonModule],
   templateUrl: './feedback-list.component.html',
   styleUrl: './feedback-list.component.scss'
 })
 export class FeedbackListComponent {
 
-  feedback:any []=[]
-    constructor(public api: ApiService){}
+  feedback: any[] = [];
+  filteredFeedback: any[] = [];
+  currentFilter: string = 'all';
 
-    ngOnInit(){
-      this.api.getUserFeedback().subscribe((res:any)=>{
-       this.feedback = res;
-        console.log(this.feedback)
-      })
+  constructor(public api: ApiService) {}
 
+  ngOnInit() {
+    this.loadFeedback();
+  }
+
+  loadFeedback() {
+    this.api.getUserFeedback().subscribe((res: any) => {
+      this.feedback = res;
+      this.filteredFeedback = [...res];
+    });
+  }
+
+  onFilterChange(filter: string) {
+    this.currentFilter = filter;
+
+    if (filter === 'all') {
+      this.filteredFeedback = [...this.feedback];
+    } else if (filter === 'website') {
+      this.filteredFeedback = this.feedback.filter(item =>
+        item.productName.toLowerCase() === 'website'
+      );
+    } else if (filter === 'product') {
+      this.filteredFeedback = this.feedback.filter(item =>
+        item.productName.toLowerCase() !== 'website'
+      );
     }
   }
+
+  shouldShowProductColumn(): boolean {
+    return this.currentFilter !== 'website';
+  }
+
+}
 
 
 
