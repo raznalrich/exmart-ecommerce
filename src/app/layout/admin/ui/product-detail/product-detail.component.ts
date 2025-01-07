01ce9efe3bdd1f477c95530f9f2d8857c2ddf837
Hithesh Lal <hithesh.lal@experionglobal.local>
2025-01-07 16:24:58 +0530

    Background blur when modal display, Button tootip for download and upload excel and list only particular order item when click on action button

--- src/app/layout/admin/ui/product-detail/product-detail.component.ts
+++ src/app/layout/admin/ui/product-detail/product-detail.component.ts
@@ -1,14 +1,22 @@
-import { CurrencyPipe } from '@angular/common';
+import { CommonModule, CurrencyPipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe,CommonModule],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.scss'
 })
 export class ProductDetailComponent {
 @Input() OrderDetailsByID : any
+@Input() selectedOrderItemId :any;
+
+get filteredOrderItem() {
+  if (!this.OrderDetailsByID?.orderItems) return null;
+  return this.OrderDetailsByID.orderItems.find(
+    (item: any) => item.orderItemId === this.selectedOrderItemId
+  );
+}
 
 }
