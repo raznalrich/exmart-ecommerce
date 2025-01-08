058c0978b47db15c6bfd61ff589cba832891d0e7
Mohammed Rasnal K A <mohammed.ka@experionglobal.local>
2025-01-08 19:08:50 +0530

    orders

--- src/app/layout/user/pages/thankyoupage/thankyoupage.component.ts
+++ src/app/layout/user/pages/thankyoupage/thankyoupage.component.ts
@@ -12,6 +12,10 @@ import { GlobalService } from '../../../../global.service';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { ApiService } from '../../../../api.service';
 import { forkJoin } from 'rxjs';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+import { ThankYouPurchaseComponent } from "../../ui/thank-you-purchase/thank-you-purchase.component";
+import { OrderEmailContext } from '../../interfaces/OrderEmailContext';
+
 interface ProductDetails {
   id: number;
   name: string;
@@ -40,14 +44,19 @@ interface Item {
 @Component({
   selector: 'app-thankyoupage',
   standalone: true,
-  imports: [RouterLink,ItemsInOrderComponent, OrderConfirmationTickAnimationComponent, LongButtonComponent, CurrencyPipe, WebFeedbackSectionComponent],
+  imports: [RouterLink, ItemsInOrderComponent, OrderConfirmationTickAnimationComponent, LongButtonComponent, CurrencyPipe, WebFeedbackSectionComponent, ThankYouPurchaseComponent],
   templateUrl: './thankyoupage.component.html',
-  styleUrl: './thankyoupage.component.scss'
+  styleUrl: './thankyoupage.component.scss',
+
 })
 export class ThankyoupageComponent {
+  isVisible = true;
+  shippingCharge:number=0;
+  orderContexts:OrderEmailContext[]=[];
   orderPlaced:string='Order Placed'
   productDetails:any
  @Input() orderContext:any;
+ userId:number=0;
 id:any
 data:any
 cartItemList:any
@@ -59,16 +68,23 @@ totalPrice: number = 0;
 
   constructor(public api: ApiService,public apis:ApiServiceService, private route: ActivatedRoute,public global:GlobalService, private router: Router,
       private animationStateService:AnimationStateService
-  ){}
+  ){
+    this.global.getUserId();
+  }
   CartItems: ProductDetails[] = []; // Array to store fetched product details
 productIds: number[] = [];
   ngOnInit(){
+    this.userId = this.global.userId();
     if(this.global.selectedAddressId()){
       this.selectedAddress = this.global.selectedAddressId();
     }
     const cartItems = this.global.signalCartList();
     this.cartItemList = this.global.signalCartList();
     this.productIds = cartItems.map(item => item.productId);
+    console.log('order context',this.global.orderContext());
+    this.orderContexts = this.global.orderContext();
+    console.log('order contexts',this.orderContexts);
+
 
     console.log('Cart list:', cartItems);
     console.log('Product IDs:', this.productIds);
@@ -89,7 +105,21 @@ productIds: number[] = [];
 
       });
     }
+    onDelete() {
 
+      this.apis.deleteCartById(this.userId).subscribe({
+        next: () => {
+          // this.refreshAddressList();
+          console.log('cart cleared');
+          this.router.navigate(['/']);
+          this.global.getCartCount();
+        },
+        error: (error) => {
+          console.error('Error deleting address', error);
+        }
+      });
+
+  }
 
   fetchCartItems(cartItems: any[]) {
       const requests = this.productIds.map((id) => this.apis.getProductsById(id));
