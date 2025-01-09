ba82af377fb4e075b1e0bd3889b611a75758e5b2
Akshay Vijayakumar <akshay.vijayakumar@experionglobal.local>
2025-01-09 18:58:18 +0530

    fix: fixed scroll view in feedback list

--- src/app/layout/admin/ui/feedback-list/feedback-list.component.ts
+++ src/app/layout/admin/ui/feedback-list/feedback-list.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../../../../api.service';
-import { FeedbackReplayComponent } from "../feedback-replay/feedback-replay.component";
 import { ProductFeedbackToggleComponent } from "../product-feedback-toggle/product-feedback-toggle.component";
 import { CommonModule } from '@angular/common';
 
@@ -9,7 +8,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-feedback-list',
   standalone: true,
-  imports: [FeedbackReplayComponent, ProductFeedbackToggleComponent,CommonModule],
+  imports: [ProductFeedbackToggleComponent, CommonModule],
   templateUrl: './feedback-list.component.html',
   styleUrl: './feedback-list.component.scss'
 })
@@ -33,13 +32,7 @@ export class FeedbackListComponent {
     });
   }
 
-  // openModal() {
-  //   const modalElement = document.getElementById('staticBackdrop');
-  //   if (modalElement) {
-  //     const modal = new bootstrap.Modal(modalElement);
-  //     modal.show();
-  //   }
-  // }
+
   onFilterChange(filter: string) {
     this.currentFilter = filter;
 
