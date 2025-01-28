0e4507ddf9da67e24f6ce3d07cb5b577ba5ac7a0
hithesh-lal <hithesh.lal@experionglobal.com>
2025-01-28 13:10:27 +0530

    role based access completed

--- src/app/authGuard/auth.guard.ts
+++ src/app/authGuard/auth.guard.ts
@@ -1,4 +1,4 @@
-import { CanActivate, CanActivateFn, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth/auth.service';
 import { Injectable } from '@angular/core';
 
@@ -8,13 +8,40 @@ import { Injectable } from '@angular/core';
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
 
-  canActivate(): boolean {
+  canActivate(route: ActivatedRouteSnapshot): boolean {
     if (!this.authService.isLoggedIn()) {
       console.log('AuthGuard: User not logged in. Redirecting to login...');
       this.router.navigate(['/login']);
       return false;
     }
-    console.log('AuthGuard: User is logged in. Access granted.');
+    // console.log('AuthGuard: User is logged in. Access granted.');
+    // return true;
+
+   // Get expected role from route data
+   const expectedRole = route.data['role'];
+   if (!expectedRole) {
+     // If no role is required, just check authentication
+     return true;
+   }
+
+   const userRole = this.authService.getUserRole();
+    console.log('Current user role:', userRole);
+    console.log('Expected role:', expectedRole);
+
+    // Check if user has the required role
+    if (userRole?.toLowerCase() !== expectedRole.toLowerCase()) {
+      console.log('AuthGuard: Invalid role. Redirecting...');
+      // Redirect based on user's role
+      if (userRole?.toLowerCase() === 'admin') {
+        this.router.navigate(['/admin/admindashboard']);
+      } else {
+        this.router.navigate(['/home']);
+      }
+      return false;
+    }
+
+    console.log('AuthGuard: Access granted.');
     return true;
-  }
+    
+}
 }
